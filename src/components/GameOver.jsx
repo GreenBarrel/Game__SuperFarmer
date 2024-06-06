311c554649a600a65ff3792d82bbbb870d6753cf
GreelBarrel <rafalwas@protonmail.com>
2024-06-06 22:20:35 +0200

    add style to game over container

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,22 +1,9 @@
+import "../style/game_over.css";
+
 function GameOver({ setStart, winningPlayer }) {
   return (
     <>
-      <span
-        style={{
-          position: "absolute",
-          zIndex: "1",
-          color: "red",
-          width: "95vw",
-          height: "90vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          fontSize: "48px",
-          backdropFilter: "blur(5px)",
-          borderStyle: "solid",
-        }}
-      >
+      <span className="game_container__game_over">
         <div>{`Player ${winningPlayer} win the game!`}</div>
         <button onClick={() => setStart(false)}>Start new game</button>
       </span>
